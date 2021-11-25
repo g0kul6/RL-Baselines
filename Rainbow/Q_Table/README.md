3cc25577ca7e0b0d6c76883db62ec473e5425e6d
Gokul <75981297+g0kul6@users.noreply.github.com>
2021-11-25 20:44:10 +0530

    Rainbow

---
+++ Rainbow/Q_Table/README.md
@@ -0,0 +1,3 @@
+# Q Table
+ 
+<img align='center' src='https://miro.medium.com/max/534/1*NvvRn59pz-D1iSkBWpuIxA.png' width='600"'>
---
+++ Rainbow/Q_Table/config.py
@@ -0,0 +1,8 @@
+import gym
+env= gym.make('FrozenLake-v0')
+episodes= 1000
+learning_rate= 0.5
+gamma= 0.99
+max_epsilon=elsilon= 1.0
+min_epsilon= 1e-2
+decay_rate= 0.005
