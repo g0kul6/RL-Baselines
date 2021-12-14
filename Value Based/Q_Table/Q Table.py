3365cf9e7aea2eaa8986be6f087b54c03ded96b6
Gokul <75981297+g0kul6@users.noreply.github.com>
2021-12-14 23:06:31 +0530

    Q_table

--- Value Based/Q_Table/Q Table.py
+++ Value Based/Q_Table/Q Table.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 from config import *
 
 #geting action space ad observation space dimensions for q_table
@@ -6,7 +7,25 @@ action_input=env.action_space.n
 observation_input=env.observation_space.n
 
 #creating the Q_table
-q_table=np.zeros((action_input,observation_input))
-
-
-
+q_table=np.zeros((observation_input,action_input))
+rewards=[]
+for episode in range(episodes):
+    state=env.reset()
+    done=False
+    total_reward=0
+    for step in range(max_step):
+        #epsiolon greedy policy
+        if np.random.uniform(0,1)>epsilon:
+            action=np.argmax(q_table[state,:])
+        else:
+            action=env.action_space.sample()
+        
+        next_state,reward,done,_=env.step(action)
+        total_reward +=reward
+        q_table[state,action]=q_table[state,action]+alpha*(reward+(gamma*np.max(q_table[next_state,:]))-q_table[state,action])
+        state=next_state
+        if done:
+            break
+    epsilon=min_epsilon+(max_epsilon-min_epsilon)*np.exp(-1*decay_rate*episode)
+    print(total_reward)
+print(q_table)
--- Value Based/Q_Table/config.py
+++ Value Based/Q_Table/config.py
@@ -1,8 +1,12 @@
 import gym
-env= gym.make('FrozenLake-v0')
-episodes= 1000
-learning_rate= 0.5
+env= gym.make('FrozenLake-v1')
+
+episodes= 10000
+max_step=100
+
+alpha= 0.5
 gamma= 0.99
-max_epsilon=elsilon= 1.0
+
+max_epsilon=epsilon= 1.0
 min_epsilon= 1e-2
 decay_rate= 0.005
