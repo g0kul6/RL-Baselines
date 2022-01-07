41dcbb86d7f3f9d12b2ffbb0088897a92e830536
g0kul6 <gokulknms6@gmail.com>
2022-01-07 21:53:02 +0530

    fine tunning alpha and gamma

--- Value Based/Q_Table/Q Table.py
+++ Value Based/Q_Table/Q Table.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from config import *
+import matplotlib.pyplot as plt
 
 #geting action space ad observation space dimensions for q_table
 action_input=env.action_space.n
@@ -8,7 +9,11 @@ observation_input=env.observation_space.n
 
 #creating the Q_table
 q_table=np.zeros((observation_input,action_input))
+
+#iterative Q_learning with epsilon greedy policy to select actions
 rewards=[]
+e=[]
+r=[]
 for episode in range(episodes):
     state=env.reset()
     done=False
@@ -22,10 +27,22 @@ for episode in range(episodes):
         
         next_state,reward,done,_=env.step(action)
         total_reward +=reward
+        #bellman equation with TD target
         q_table[state,action]=q_table[state,action]+alpha*(reward+(gamma*np.max(q_table[next_state,:]))-q_table[state,action])
         state=next_state
         if done:
             break
+    rewards.append(total_reward)
+    #decay epsilon to reduce regret in log(t)
     epsilon=min_epsilon+(max_epsilon-min_epsilon)*np.exp(-1*decay_rate*episode)
-    print(total_reward)
-print(q_table)
+    if episode%30==0:
+        print("episode: ",episode)
+        print("Best Reward: ",max(rewards))
+        print("Mean reward over last 30: ",np.mean(rewards))
+        print("epsilon: ",epsilon)
+        e.append(episode)
+        r.append(np.mean(rewards))
+        rewards=[]
+
+plt.plot(e,r)
+plt.show()
